3755a856b5c0f4ec12d73a1620cbfb6bce625af9
Luiz <luizh10@uniplaclages.edu.br>
2023-11-25 11:28:38 -0300

    segundo commit cs

---
+++ segundo.cs
@@ -0,0 +1,12 @@
+using system;
+
+namespace oitoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            console.writeline("olá, mundo!");
+        }
+    }
+}
